06f144d654c0fcf8b40c094df479fda58be7a6e5
Yi-love <438002896@qq.com>
2017-12-13 20:10:43 +0800

    add prefect

--- readme.md
+++ readme.md
@@ -11,3 +11,20 @@ ice('03878').then((result)=>{
     console.log('error: ' , err);
 });
 ```
+
+log:
+
+```js
+{ 
+  perfect: 1,
+  data:
+  { 
+    companyUrl: 'http://www.vicon.com.hk/',
+    lotSize: 2000,
+    price: [ 1, 1.2 ],
+    ipoTime: { startTime: [Array], endTime: [Array] },
+    publishTime: [ '12', '21' ],
+    boardTime: [ '17', '12', '22' ] 
+  } 
+}
+```
--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { URL } = require('url');
+
 const cheerio = require('cheerio');
 
 function checkData( $ ){
@@ -14,7 +16,8 @@ function getInformation( $ ) {
         let informationArr = $('.figureTable'); 
         let baseArr = informationArr.eq(0).find('tr');
 
-        let companyUrl = baseArr.eq(2).find('a').eq(0).text().trim();
+        let companyUrl = baseArr.eq(2).find('a').eq(0).attr('href').trim();
+                
         let lotSize = baseArr.eq(3).find('.TableNumBlack').eq(0).text().trim();
 
         let stockArr = informationArr.eq(1).find('tr');
@@ -34,11 +37,39 @@ function getInformation( $ ) {
     return {perfect , data};
 }
 
+function getPerfectInformation(result){
+    let information = {};
+    try{    
+        let data = result.data;
+        information.companyUrl = new URL(data.companyUrl).href;
+        information.lotSize = Math.round(+(('' + data.lotSize).replace(/[^0-9]/g , '')));
+        information.price = ('' + data.price).replace(/[^0-9.,]/g , ' ').replace(/\s+/g,' ').replace(/,/g , '').trim().split(' ');
+        information.price[0] = +information.price[0];
+        information.price[1] = +information.price[1];
+
+        information.ipoTime = data.ipoTime.match(/(\d+年\s*)?\d+月\s*\d+日\s*/g);
+        information.ipoTime = {
+            startTime: ('' + information.ipoTime[0]).replace(/[^0-9]/g , ' ').replace(/\s+/g,' ').trim().split(' '),
+            endTime: ('' + information.ipoTime[1]).replace(/[^0-9]/g , ' ').replace(/\s+/g,' ').trim().split(' ')
+        }
+
+        information.publishTime = data.publishTime.match(/(\d+年\s*)?\d+月\s*\d+日\s*/g)[0];
+        information.publishTime = information.publishTime.replace(/[^0-9]/g , ' ').replace(/\s+/g,' ').trim().split(' ');
+
+        information.boardTime = data.boardTime.match(/(\d+年\s*)?\d+月\s*\d+日\s*/g)[0];
+        information.boardTime = information.boardTime.replace(/[^0-9]/g , ' ').replace(/\s+/g,' ').trim().split(' ');
+    }catch(e){
+        return {perfect:0};
+    }
+    return {perfect:1 , data:information};
+}
+
 exports.parse = ( html = '' )=>{
     const $ = cheerio.load(html);
 
     if ( !checkData($) ){
         return {perfect: 0}
     }
-    return getInformation($);
+    let result = getInformation($);
+    return getPerfectInformation(result);
 }
